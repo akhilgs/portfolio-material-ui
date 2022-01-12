302cfd26dd7e86402b45a886f3b9d9b4eee05383
Akhil Gangarpu Sudhakar <akhil.clk93@gmail.com>
2022-01-11 21:29:38 -0500

    Updates

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
-    printWidth: 100,
-    editorWordWrap: 'wordWrapColumn',
-    editorWordWrapColumn: 100,
-}
